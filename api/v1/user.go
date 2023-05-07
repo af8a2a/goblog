740111d7fd976845519272be4c7157be8882ed82
af8a2a <1125209282@qq.com>
2023-05-07 12:03:15 +0800

    编辑用户

--- api/v1/user.go
+++ api/v1/user.go
@@ -10,11 +10,6 @@ import (
 
 var code int
 
-// 查询用户是否存在
-func UserExist(c *gin.Context) {
-
-}
-
 // 添加用户
 func AddUser(c *gin.Context) {
 	//todo 添加用户
@@ -40,6 +35,7 @@ func AddUser(c *gin.Context) {
 func GetUsers(c *gin.Context) {
 	pageSize, _ := strconv.Atoi(c.Query("pagesize"))
 	pageNum, _ := strconv.Atoi(c.Query("pagenum"))
+	_ = c.ShouldBindJSON("")
 	if pageSize == 0 {
 		pageSize = -1
 	}
@@ -58,10 +54,32 @@ func GetUsers(c *gin.Context) {
 
 // 编辑用户
 func EditUser(c *gin.Context) {
-
+	var data model.User
+	id, _ := strconv.Atoi(c.Param("id"))
+	_ = c.ShouldBindJSON(&data)
+	code = model.CheckUser(data.Username)
+	//重命名的名字不能重复
+	if code == errmsg.SUCCSE {
+		model.EditUser(id, &data)
+	}
+	if code == errmsg.ERROR_USERNAME_USED {
+		c.Abort()
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 // 删除用户
 func DeleteUser(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	code = model.Delete(id)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
 
 }
